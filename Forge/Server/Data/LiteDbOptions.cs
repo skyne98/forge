5a32058041266980a2dadec5d2a348a2877fc94c
skyne98 <ahalahan@gmail.com>
2020-10-16 18:19:08 +0200

    Fix the db location

--- Forge/Server/Data/LiteDbOptions.cs
+++ Forge/Server/Data/LiteDbOptions.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Forge.Server.Data
 {
     public class LiteDbOptions
     {
-        public string DatabaseLocation { get; set; }
+        public string DatabaseLocation { get; set; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database.db");
     }
 }
